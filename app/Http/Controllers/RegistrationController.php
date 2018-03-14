cb4f5353bfd1249f7a868cca122d32dab3cc8b66
Rick Pfahl <pfahlr@gmail.com>
2018-03-14 02:31:36 -0400

    Form Request Objects

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,38 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\User;
-use App\Mail\Welcome;
-
+use App\Http\Requests\RegistrationForm;
 
 class RegistrationController extends Controller
 {
-    //
   public function create()
   {
     return view('registrations.create');
   }
 
-  public function store()
+  public function store(RegistrationForm $form)
   {
-    //validate
-    $this->validate(request(), [
-      'name'=>'required|min:2',
-      'email'=>'required|email',
-      'password'=>'required|confirmed'
-    ]);
-
-    //create
-    $arr = request(['name','email','password']);
-    $arr['password'] = bcrypt($arr['password']);
-    $user = User::create($arr);
-
-    //login
-    auth()->login($user);
-    \Mail::to($user)->send(new Welcome($user));
-
+    $form->persist();
     return redirect()->route('home_page');
-
   }
 }
---
+++ app/Http/Requests/RegistrationForm.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\User;
+use App\Mail\Welcome;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Foundation\Http\FormRequest;
+
+
+class RegistrationForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+          'name'=>'required|min:2',
+          'email'=>'required|email',
+          'password'=>'required|confirmed'
+        ];
+    }
+
+    public function persist()
+    {
+      //create
+      $arr = $this->only(['name','email','password']);
+      $arr['password'] = bcrypt($arr['password']);
+      $user = User::create($arr);
+
+      //login
+      auth()->login($user);
+      Mail::to($user)->send(new Welcome($user));
+    }
+}
